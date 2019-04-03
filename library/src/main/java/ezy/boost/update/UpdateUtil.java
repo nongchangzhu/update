c26eb0d61922a69b53838fe797b25405a3a08e6a
shamine <>
2019-04-03 18:23:58 +0800

    fix force update crash

--- library/src/main/java/ezy/boost/update/UpdateUtil.java
+++ library/src/main/java/ezy/boost/update/UpdateUtil.java
@@ -16,6 +16,7 @@
 
 package ezy.boost.update;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -27,7 +28,6 @@ import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Build;
 import android.support.v4.content.FileProvider;
-import android.support.v7.app.AlertDialog;
 import android.text.TextUtils;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
